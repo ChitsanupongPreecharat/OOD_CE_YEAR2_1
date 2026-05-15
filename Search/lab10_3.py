26f3be787134dda1f9801a1696a88b49d3e8cc8f
Chitsanupong Preecharat <chitsanupongpreecharat@gmail.com>
2026-05-15 14:54:17 +0700

    update github from agentic ai (codex)

--- Search/lab10_3.py
+++ Search/lab10_3.py
@@ -4,58 +4,59 @@ class Data:
         self.value = value
 
     def __str__(self):
-        return "({0}, {1})".format(self.key, self.value)
+        return f"({self.key}, {self.value})"
 
-class hash:
-    def __init__(self,size,max_collision):
+
+class HashTable:
+    def __init__(self, size, max_collision):
         self.size = size
         self.max_collision = max_collision
         self.table = [None] * size
         self.full_msg_shown = False
-    
-    def get_index(self,key):
+
+    def get_index(self, key):
         total = sum(ord(ch) for ch in key)
         return total % self.size
-    
-    def insert(self,data):
-        if not self.full_msg_shown:
-            if all(self.table):
-                if not self.full_msg_shown:
-                    print("This table is full !!!!!!")
-                    self.full_msg_shown = True
-                    return
-            
-            index = self.get_index(data.key)
-            i = 0
-            while i < self.max_collision:
-                new_index = (index + i**2) % self.size
-                if self.table[new_index] is None:
-                    self.table[new_index] = data
-                    return 
-                else:
-                    print(f"collision number {i+1} at {new_index}")
-                    i += 1
-            print("Max of collisionChain")
 
-        
-            
-    
+    def is_full(self):
+        return all(item is not None for item in self.table)
+
+    def insert(self, data):
+        if self.full_msg_shown:
+            return
+
+        if self.is_full():
+            print("This table is full !!!!!!")
+            self.full_msg_shown = True
+            return
+
+        index = self.get_index(data.key)
+
+        for i in range(self.max_collision):
+            new_index = (index + i**2) % self.size
+
+            if self.table[new_index] is None:
+                self.table[new_index] = data
+                return
+
+            print(f"collision number {i + 1} at {new_index}")
+
+        print("Max of collisionChain")
+
     def show_information(self):
         if not self.full_msg_shown:
-            for i,item in enumerate(self.table):
+            for i, item in enumerate(self.table):
                 print(f"#{i+1}	{item}")
             print("---------------------------")
-        
 
 
 print(" ***** Fun with hashing *****")
-infor,List = input("Enter Input : ").split('/')
-size_table,max_cliiision = map(int,infor.split())
-List = list(List.split(','))
-H = hash(size_table,max_cliiision)
+info, data_list = input("Enter Input : ").split("/")
+size_table, max_collision = map(int, info.split())
+data_list = data_list.split(",")
+H = HashTable(size_table, max_collision)
 
-for l in List:
-    key,value = l.split()
-    H.insert(Data(key,value))
+for item in data_list:
+    key, value = item.split()
+    H.insert(Data(key, value))
     H.show_information()
-    
